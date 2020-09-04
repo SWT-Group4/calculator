358c4d1f4dbe996c15e3de142db97391c06a62a0
Daniel Vinther Wolf <danielvinther@youmail.dk>
2020-09-04 10:33:06 +0200

    Refine CalculatorUnitTests

--- 01_Calculator.Test.Unit/01_CalculatorUnitTests.cs
+++ 01_Calculator.Test.Unit/01_CalculatorUnitTests.cs
@@ -3,20 +3,20 @@ using NUnit.Framework;
  *  File: 01_CalculatorUnitTests.cs
  *  Author: Daniel K. Vinther Wolf
  *  Created: 2020-09-03
- *  Last Modified: -
- *  Version: 0.1.0
+ *  Last Modified: 2020-09-04
+ *  Version: 0.1.1
  */
 
-/*
- * Method: Boundary Value Analysis (bs) or Eq. Classes (eq)
+/* Description: "Test Strategy" and Establishment of Unit Test Cases:
+ * Method: Boundary Value Analysis (bv) or Eq. Classes (eq)
  *  ID  | Function          | Valid and Invalid classes
  * -----------------------------------------------------------------------
  *  01  | Add (a + b)       | V: real numbers / I: non-numbers
  *      <<--- Test Cases --->>      |   Testdata            | Expected
  *  eq  01.1  CanAddNegWithPos:         a=(-1.0), b=1.0       0.00
- *  bs  01.2  WillNotRound_2:           a=1.25, b=1.24        2.49
- *  bs  01.3  WillNotRound_3:           a=1.26, b=1.24        2.50
- *  bs  01.4  WillNotRound_4:           a=1.27, b=1.24        2.51
+ *  bv  01.2  WillNotRound_2:           a=1.25, b=1.24        2.49
+ *  bv  01.3  WillNotRound_3:           a=1.26, b=1.24        2.50
+ *  bv  01.4  WillNotRound_4:           a=1.27, b=1.24        2.51
  * -----------------------------------------------------------------------
  *  02  | Subtract (a - b)  | V: real numbers / I: non-numbers
  *      <<--- Test Cases --->>      |   Testdata            | Expected
